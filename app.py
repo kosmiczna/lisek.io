3ade89cf9610662c9a35d76b0442ac4edc1d6e5d
paw <ashley@pawstar.pl>
2026-07-22 03:50:23 +0100

    basic flask implementation, and placeholder index with a few links

---
+++ app.py
@@ -0,0 +1,18 @@
+# lisek.io
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# main page
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
