1aa7ec738612a4ee55824ecfdda21a9aef23cfd0
paw <ashley@pawstar.pl>
2026-07-22 05:20:55 +0100

    made url shortner (very simple)
    Signed-off-by: paw <ashley@pawstar.pl>

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 # lisek.io
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+import json
 
 app = Flask(__name__)
 
+
+with open("links.json", "r") as file:
+    links = json.load(file)
 
 # main page
 @app.route("/")
@@ -14,7 +18,11 @@ def index():
 # url shortner
 @app.route("/<link>")
 def link(link):
-    return link
+    for entry in links:
+        if link in entry:
+            return redirect(entry[link])
+    else:
+        return "not found"
 
 if __name__ == "__main__":
     app.run(debug=True)
